20b31acdc3e6b671fdabaf7b92e66c43747853ce
Sanjeev Sharma <sanjeev.sharma90@myhunter.cuny.edu>
2018-09-22 17:17:01 -0400

    Pulling Expectations from written code

--- identify-expectation.js
+++ identify-expectation.js
@@ -9,3 +9,26 @@
 const homeCriteria = (size, type, city) => `I am looking for a ${size} bedroom ${type} in ${city}`;
 
 console.log(homeCriteria(3, 'Apartment', 'NYC'));
+
+
+/*
+    Now try to do the same in reverse. Given the following code, figure out the expectations of it:
+*/
+const reverseString = (str) => {
+    let reversedString = "";
+  
+    for(let lastIndex = str.length -1; lastIndex >= 0; lastIndex--) {
+      reversedString += str[lastIndex];
+    }
+  
+    return reversedString;
+  }
+  
+  reverseString('StrangerThings');
+
+/*
+    1. We expect reverseString to be a function
+    2. We expect reverseString to return a string
+    3. We expect reverseString to accept a string argument
+    4. We expect reverseString to return the reverse of the accepted string argument        
+*/
