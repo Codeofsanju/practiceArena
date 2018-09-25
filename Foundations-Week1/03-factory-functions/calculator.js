965164191d8140fe7c12d568dee9f2f38e24f5db
Sanjeev Sharma <sanjeev.sharma90@myhunter.cuny.edu>
2018-09-24 22:03:54 -0400

    03-Factory-Functions - createCalculator (Done)

--- Foundations-Week1/03-factory-functions/calculator.js
+++ Foundations-Week1/03-factory-functions/calculator.js
@@ -1,1 +1,17 @@
-
+const createCalculator = () => {
+    return{
+        curr: 0,
+        value() {
+            return this.curr;
+        },
+        add(n){
+            this.curr += n;
+        },
+        subtract(n){
+            this.curr -= n;
+        },
+        clear(){
+            this.curr = 0;
+        }
+    }
+}
