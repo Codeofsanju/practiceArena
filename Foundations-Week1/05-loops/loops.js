245c163f32f363389cfd9abec70fa53f85b2f316
Sanjeev Sharma <sanjeev.sharma90@myhunter.cuny.edu>
2018-09-27 17:56:28 -0400

    05-LOOPS - all array/string looping problems done

--- Foundations-Week1/05-loops/loops.js
+++ Foundations-Week1/05-loops/loops.js
@@ -1,4 +1,5 @@
 
+// CREATE REPEATING STRING WITH NUMBER OF REPEATS
 const repeat = (string, number) => {
     let ans = '';
     for(let i = 0; i < number; i++){
@@ -7,6 +8,7 @@ const repeat = (string, number) => {
     return ans;
 }
 
+// SUM ALL ARRAY NUMBERS
 const sum = (arr = []) => {
     let ans = 0;
     let i = 0;
@@ -17,6 +19,7 @@ const sum = (arr = []) => {
     return ans;
 }
 
+// JOIN ARRAY TO STRING WITH AND WITHOUT DELIMITER
 const join = (arr = [], delim = '') =>{
     let ans = '';
     for(let i = 0; i < arr.length; i++){
@@ -33,6 +36,41 @@ const join = (arr = [], delim = '') =>{
             ans += arr[i]+delim;
         }
     }
-    console.log(ans);
     return ans;
 }
+
+// GRID GENERATOR
+const gridGenerator = (n) =>{
+    let ans = '';
+    if(n === 0){
+        return ans;
+    }
+    for(let a = 0; a < n; a++){
+        if (a > 0){
+            ans+='\n';
+        }
+        for(let b = 0; b < n; b++){
+            if(a%2 ===0){
+                if(b%2 ===0){
+                    ans+='#';
+                }
+                else{
+                    ans+=' ';
+                }
+            }
+            else if(a%2 !== 0){
+                if(b%2 !== 0){
+                    ans+='#';
+                }
+                else{
+                    ans+=' ';
+                }
+            }
+        }
+    }
+    console.log(ans);
+    ans+='\n';
+    return ans;
+}
+// a going down
+// b going across
