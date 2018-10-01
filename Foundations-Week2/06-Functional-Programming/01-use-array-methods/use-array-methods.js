85950dfb30bb810fb046c298c95c070df8ed3c31
Sanjeev Sharma <sanjeev.sharma90@myhunter.cuny.edu>
2018-10-01 17:38:34 -0400

    01-use-array-methods - indexValue, swapCase

--- Foundations-Week2/06-Functional-Programming/01-use-array-methods/use-array-methods.js
+++ Foundations-Week2/06-Functional-Programming/01-use-array-methods/use-array-methods.js
@@ -1,1 +1,21 @@
 
+// indexAndValue: using map, return an array of object. Add value, and index properties
+const indexAndValue = (arr) =>{
+    return arr.map((value, index) =>{
+        return {value, index};
+    })
+}
+
+//swapCase:  
+//create the method capitilize that tales a string and capitalizes the string
+const capitalize = (word) => word.toUpperCase();
+const swapCase = (string) =>{
+    const newArr = string.split(' ');
+    return newArr.map((value, index) => {
+        if(index%2 === 0){
+            return capitalize(value);
+        }
+        return value;
+    })
+    .join(' ');
+}
