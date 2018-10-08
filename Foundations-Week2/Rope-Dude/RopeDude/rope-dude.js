f3f1bea80148a1af2c0cb9c83174c8c2c0b4e25a
Sanjeev Sharma <sanjeev.sharma90@myhunter.cuny.edu>
2018-10-08 16:32:10 -0400

    Stylistic changes to RopeDude

--- Foundations-Week2/Rope-Dude/RopeDude/rope-dude.js
+++ Foundations-Week2/Rope-Dude/RopeDude/rope-dude.js
@@ -60,64 +60,62 @@ const ASCIIART = [
 ];
 
 class RopeDude {
-    constructor(secwrd){  
-      this.secretWord = secwrd.split('');
-      this.remainingGuesses = 6;
-      this.lettersGuessed = [];
-      this.gameState = 'playing';
+  constructor(secwrd) {
+    this.secretWord = secwrd.split('');
+    this.remainingGuesses = 6;
+    this.lettersGuessed = [];
+    this.gameState = 'playing';
+  }
+  submitGuess(guess) {
+    guess = guess.toLowerCase();
+    if (this.gameState !== 'playing') return;
+    !this.lettersGuessed.includes(guess) && this.lettersGuessed.push(guess);
+    !this.secretWord.includes(guess) && this.remainingGuesses--;
+    this.computeGameState();
+  }
+  computeGameState() {
+    if (this.remainingGuesses === 0) this.gameState = 'lost';
+    if (this.secretWord.every(n => this.lettersGuessed.includes(n))) {
+      this.gameState = 'won';
     }
-    submitGuess(guess){
-      guess = guess.toLowerCase();
-      if (this.gameState !== 'playing')  return;
-      !this.lettersGuessed.includes(guess) && this.lettersGuessed.push(guess);
-      !this.secretWord.includes(guess) && this.remainingGuesses--;
-      this.computeGameState();
-    }
-    computeGameState(){
-      if(this.remainingGuesses === 0) this.gameState = 'lost';
-      if(this.secretWord.every((n) => this.lettersGuessed.includes(n))){
-        this.gameState = 'won';
-      }
-    }
+  }
 
-    getSecretWordPuzzle(){
-      let ans = this.secretWord.map(n => n !== ' ' ? '#' : ' ');
-      let check = this.secretWord.filter(n => this.lettersGuessed.includes(n))
-      for(let i = 0; i < check.length; i++){
-        for(let j = 0; j < this.secretWord.length; j++){
-          if(check[i] === this.secretWord[j]){
-            ans[j] = check[i];
-          }
+  getSecretWordPuzzle() {
+    let ans = this.secretWord.map(n => (n !== ' ' ? '#' : ' '));
+    let check = this.secretWord.filter(n => this.lettersGuessed.includes(n));
+    for (let i = 0; i < check.length; i++) {
+      for (let j = 0; j < this.secretWord.length; j++) {
+        if (check[i] === this.secretWord[j]) {
+          ans[j] = check[i];
         }
       }
-      return ans.join('');
     }
+    return ans.join('');
+  }
 
-    getGameStateMessage(){
-      if(this.gameState === 'playing'){
-        return `There is a total of ${6 - this.lettersGuessed.length} guesses remaining:\n${ASCIIART[6 - this.lettersGuessed.length]}`;
-      }
-      else if(this.gameState === 'lost'){
-        return `Game Over, the word was "${this.secretWord.join('')}":\n${ASCIIART[0]}`;
-      }
-      else if(this.gameState === 'won'){
-        return `Winner Winner Chicken Dinner, you won!`;
-      }
+  getGameStateMessage() {
+    if (this.gameState === 'playing') {
+      return `There is a total of ${
+        this.remainingGuesses
+      } guesses remaining:\n${ASCIIART[this.remainingGuesses]}`;
+    } else if (this.gameState === 'lost') {
+      return `Game Over, the word was "${this.secretWord.join('')}":\n${
+        ASCIIART[0]
+      }`;
+    } else if (this.gameState === 'won') {
+      return `Winner Winner Chicken Dinner, you won!`;
     }
-
-
+  }
 }
 
-
-
-const simulateRopeDude = () =>{
+const simulateRopeDude = () => {
   const game = new RopeDude('sanjeev');
-  game.submitGuess('s');   
-  game.submitGuess('a');   
+  game.submitGuess('s');
+  game.submitGuess('a');
   game.submitGuess('e');
   game.submitGuess('v');
- 
+
   console.log(game.getSecretWordPuzzle());
   console.log(game.getGameStateMessage());
   return game.getGameStateMessage();
-}
+};
