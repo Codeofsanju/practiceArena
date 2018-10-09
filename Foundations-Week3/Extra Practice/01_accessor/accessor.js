8cb3c736dbc50b39e006b1700e67ae5cf5f8cbb9
Sanjeev Sharma <sanjeev.sharma90@myhunter.cuny.edu>
2018-10-09 17:22:04 -0400

    01-accessor DONE

--- Foundations-Week3/Extra Practice/01_accessor/accessor.js
+++ Foundations-Week3/Extra Practice/01_accessor/accessor.js
@@ -1,3 +1,7 @@
+//Returns a function that acts as a getter/setter for the given object
+//Key concepts:
+//1: Closure
+//2: dot vs. bracket notation
 const accessor = (obj) =>{
     return function (key, value = ''){
         if(value === ''){
