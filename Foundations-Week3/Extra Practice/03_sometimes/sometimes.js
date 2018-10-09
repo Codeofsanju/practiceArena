37713428f878f29f81fb6f7f10af166b50056283
Sanjeev Sharma <sanjeev.sharma90@myhunter.cuny.edu>
2018-10-09 19:36:40 -0400

    03-sometimes (done)

--- Foundations-Week3/Extra Practice/03_sometimes/sometimes.js
+++ Foundations-Week3/Extra Practice/03_sometimes/sometimes.js
@@ -1,1 +1,10 @@
-
+const sometimes = (func) =>{
+    let count = 0;
+    return function(){
+        count++;
+        if(count >= 4 && count%2 === 0){
+            return 'I do not know!';
+        }
+        return func.apply(null, arguments);
+    };
+}
