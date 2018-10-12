68de7161869d87bd8b4c462e8847c8ae43a535fe
Sanjeev Sharma <sanjeev.sharma90@myhunter.cuny.edu>
2018-10-12 01:47:40 -0400

    12_map_quest (DONE)

--- Foundations-Week3/Extra Practice/12_map_quest/solutions_map_quest.js
+++ Foundations-Week3/Extra Practice/12_map_quest/solutions_map_quest.js
@@ -1,1 +1,15 @@
 
+const mapQuest = (arr) =>{
+    function oppFinder(dir){
+        switch(dir){
+        case 'N': return 'S'; 
+        case 'S': return 'N'; 
+        case 'E': return 'W';
+        case 'W': return 'E';
+        }
+    }
+    return arr.reduce((acumm, n) => {
+        acumm[acumm.length -1] === oppFinder(n) ? acumm.pop() : acumm.push(n);
+        return acumm;
+    },[])    
+}
