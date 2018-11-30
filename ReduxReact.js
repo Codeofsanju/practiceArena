0c31d3a4b78b6ffb82fae60fc21189687cb52566
Sanjeev Sharma <sanjeev.sharma90@myhunter.cuny.edu>
2018-11-30 11:32:13 -0500

    React+Redux - safer

--- ReduxReact.js
+++ ReduxReact.js
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-import store from './SomeFileWithStore'
+import store, {DEPOSIT} from './SomeFileWithStore'
 
 
 class Bank extends Component {
@@ -24,10 +24,24 @@ class Bank extends Component {
             <div>
                 <h1> {store.getState().balance} </h1>
                 <button onClick = {store.dispatch({type: 'DEPOSIT', amount: 100})}> Deposit 100 </button>
+                {/* OR */}
+                <button onClick = {store.dispatch(deposit(100))}> Deposit 100 </button>
             </div>
         );
     }
 }
+
+
+
+//IN store
+
+// ACTION TYPE CONSTANTS
+const DEPOSIT = 'deposit';
+
+
+// ACTION CREATORS
+const deposit = amount => ({type: DEPOSIT, amount});
+// REDUCER
 
 
 
